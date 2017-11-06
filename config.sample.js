d32e3451f86c5ff91fed623ce623ad3e65620418
Eric Wong <eric@k2.digital>
2017-11-06 15:20:11 +0800

    decoupled api into separate repo

--- config.sample.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    cookieSessionKeys: ['keykeykey'],
-    adminHash: '$2a$10$_a-bcrypt-of-`${req.body.username}:${req.body.password}`',
-}
--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const proxy = require('http-proxy-middleware');
 
 var app = express();
 
-// NOTE: serve /admin using nginx
-app.use('/api', require('./api'));
+// configure express app
+app.enable('strict routing');
+app.disable('x-powered-by');
+
+// when in development mode:
+// (1) serve admin
+// (2) proxy_pass to api on port process.env.API_PORT (or default 7777)
+// on production server these are handled by nginx
+if(process.env.NODE_ENV == 'development'){
+    console.log('running in development mode');
+    var apiPort = process.env.API_PORT || 7777;
+    app.use('/admin', express.static('../monitor-admin/build'));
+    app.use('/api', proxy(`http://127.0.0.1:${apiPort}`, {pathRewrite: {'^/api': ''}}));
+    console.log('proxy_pass-ing /api to 127.0.0.1:%s', apiPort);
+}
 
 app.listen(80);
