879d011b4557fcbcaa05d5f2ac1c9a6cc99dbedd
Eric Wong <eric@k2.digital>
2017-11-07 11:29:12 +0800

    moved materialize-scss to use local

--- index.js
+++ index.js
@@ -31,14 +31,17 @@ nunjucks.configure('views/', {
 
 
 // when in development mode:
-// (1) serve admin
+// (1) serve admin at /admin
 // (2) proxy_pass to api on port process.env.API_PORT (or default 7777)
+// (3) serve build and static at /
 // on production server these are handled by nginx
 if(process.env.NODE_ENV == 'development'){
     console.log('running in development mode');
     var apiPort = process.env.API_PORT || 7777;
     app.use('/admin', express.static('../monitor-admin/build'));
-    app.use('/api', proxy(`http://127.0.0.1:${apiPort}`, {pathRewrite: {'^/api': ''}}));
+	app.use('/api', proxy(`http://127.0.0.1:${apiPort}`, {pathRewrite: {'^/api': ''}}));
+	app.use('/', express.static('./build'));
+	app.use('/', express.static('./static'));
     console.log('proxy_pass-ing /api to 127.0.0.1:%s', apiPort);
 }
 
