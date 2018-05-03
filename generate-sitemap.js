2c45caa20b2a2db9127890407df410703182616e
Eric Wong <eric@k2.digital>
2018-05-03 21:25:39 +0800

    added robots.txt and remove /en/* from sitemap parent.njk: added html[lang]

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const sitemap = require('sitemap');
 
 var urls = [ // static urls
-    {url: '/en/', changefreq: 'daily', priority: 0.7},
+    // {url: '/en/', changefreq: 'daily', priority: 0.7},
     {url: '/zh/', changefreq: 'daily', priority: 0.7},
-    {url: '/en/deputies', changefreq: 'weekly', priority: 0.5},
+    // {url: '/en/deputies', changefreq: 'weekly', priority: 0.5},
     {url: '/zh/deputies', changefreq: 'weekly', priority: 0.5},
-    {url: '/en/bills', changefreq: 'weekly', priority: 0.7},
+    // {url: '/en/bills', changefreq: 'weekly', priority: 0.7},
     {url: '/zh/bills', changefreq: 'weekly', priority: 0.7},
-    {url: '/en/documents', changefreq: 'weekly', priority: 0.7},
+    // {url: '/en/documents', changefreq: 'weekly', priority: 0.7},
     {url: '/zh/documents', changefreq: 'weekly', priority: 0.7},
 ]
 
@@ -27,12 +27,12 @@ async.parallel({
     }
 
     result.deputies.forEach(deputy => {
-        urls.push({url: `/en/deputies/${deputy.id}/${deputy.slug}`, changefreq: 'weekly', priority: 0.5});
+        // urls.push({url: `/en/deputies/${deputy.id}/${deputy.slug}`, changefreq: 'weekly', priority: 0.5});
         urls.push({url: `/zh/deputies/${deputy.id}/${deputy.slug}`, changefreq: 'weekly', priority: 0.5});
     })
 
     result.bills.forEach(bill => {
-        urls.push({url: `/en/bills/${bill.id}/${bill.slug}`, changefreq: 'weekly', priority: 0.5});
+        // urls.push({url: `/en/bills/${bill.id}/${bill.slug}`, changefreq: 'weekly', priority: 0.5});
         urls.push({url: `/zh/bills/${bill.id}/${bill.slug}`, changefreq: 'weekly', priority: 0.5});
     })
 
